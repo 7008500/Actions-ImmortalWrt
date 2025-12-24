50a81f48907afbf40c438688893b5d087562f161
7008500 <7008500@qq.com>
2025-12-24 16:24:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,11 +2,11 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
-sed -i 's/192.168.1.1/10.0.0.1/g' ImmortalWrt/package/base-files/files/bin/config_generate
-sed -i 's/192.168./10.0./g' ImmortalWrt/package/base-files/files/bin/config_generate
-sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' ImmortalWrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/0xf60000/0x1f60000/g' ImmortalWrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/15744k/32448k/g' ImmortalWrt/target/linux/ramips/image/mt7621.mk
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168./10.0./g' package/base-files/files/bin/config_generate
+sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/0xf60000/0x1f60000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 cp -f ../.config .config

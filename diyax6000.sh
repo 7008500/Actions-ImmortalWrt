fe0fb8933836ea9bc18d2ebd86831e7682fea988
7008500 <7008500@qq.com>
2026-01-20 00:24:46 +0800

    Update diyax6000.sh

--- diyax6000.sh
+++ diyax6000.sh
@@ -4,9 +4,6 @@
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168./10.0./g' package/base-files/files/bin/config_generate
-sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/0xf60000/0x1f60000/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 cp -f ../.configax6000 .configax6000
